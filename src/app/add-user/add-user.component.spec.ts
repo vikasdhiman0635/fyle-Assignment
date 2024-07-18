5f6a971e74dd9871a113eba3e27169fa08ea6079
vikasdhiman0635 <vikasdhiman835@gmail.com>
2024-07-18 16:32:08 +0530

    add test cases

--- src/app/add-user/add-user.component.spec.ts
+++ src/app/add-user/add-user.component.spec.ts
@@ -7,6 +7,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+const storageData: any = [
+  { userName: 'test', workoutType: "Running", workoutMinutes: 20 },
+  { userName: 'test1', workoutType: "Swiming", workoutMinutes: 50 },
+  { userName: 'test2', workoutType: "Yoga", workoutMinutes: 25 }
+]
+
 describe('AddUserComponent', () => {
   let component: AddUserComponent;
   let fixture: ComponentFixture<AddUserComponent>;
@@ -33,4 +39,52 @@ describe('AddUserComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be check checkUSer method when return true', () => {
+    component.storageData = [
+      {
+        userName: 'test'
+      }
+    ]
+    const data: any = {
+      userName: 'test'
+    }
+    const return_spy = component.checkUSer(data);
+    expect(return_spy).toBe(true);
+  });
+
+  it('should be check checkUSer method when return false', () => {
+    component.storageData = [
+      {
+        userName: 'test'
+      }
+    ]
+    const data: any = {
+      userName: 'test1'
+    }
+    const return_spy = component.checkUSer(data);
+    expect(return_spy).toBe(false);
+  });
+
+  it('should be test addUserSubmit method when I dont have data in localstorage', () => {
+    component.addUser.get("userName").setValue('test');
+    // localStorage.setItem('users', JSON.stringify(storageData));
+
+    component.addUser.get("workoutType").setValue('Running');
+    component.addUser.get("workoutMinutes").setValue('30');
+    component.addUserSubmit();
+    expect(component.storageData).toBeTruthy();
+  });
+
+  it('should be test addUserSubmit method when I have data in localstorage', () => {
+    component.addUser.get("userName").setValue('test');
+    localStorage.setItem('users', JSON.stringify(storageData));
+
+    component.addUser.get("workoutType").setValue('Running');
+    component.addUser.get("workoutMinutes").setValue('30');
+    jest.spyOn(component, 'checkUserExist');
+    component.addUserSubmit();
+    expect(component.storageData).toBeTruthy();
+  });
+
 });
